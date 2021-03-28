094aacb88ff2cf2161bead3c36ee838b04da6e6a
BejlA5 <78590371+BejlA5@users.noreply.github.com>
2021-03-28 23:05:52 +0200

    Consent page almost done. Text not white.

---
+++ consent.js
@@ -0,0 +1,39 @@
+var color = "white"
+var consent_text = '<h1 style="color:' + color + '">Tájékoztató és beleegyező nyilatkozat</h1>'+'<p style="color:' + color + '>Ez a kérdőív névtelen.\
+ A kérdőívre adott válaszokkal együtt nem rögzítünk semmilyen Önnel kapcsolatos azonosító információt, csak akkor, \
+ ha kimondottan ilyen adatra vonatkozó kérdésre válaszol a kérdőívben. Ha azonosító kitöltésjelszót használt ennek a kérdőívnek az eléréséhez, \
+ megnyugodhat, hogy ezt a kitöltésjelszót a válaszoktól elkülönítetten kezeljük. A kitöltésjelszót külön adatbázisban tároljuk, és csak azt jelzi, \
+ hogy a kitöltése befejeződött ill. még nem fejeződött be. A kitöltésjelszó semmilyen módon sem kapcsolható össze a kérdőív kérdéseire adott válaszokkal.</p>'
+ +'<h2 style="color:' + color + '>ADATKEZELÉSI TÁJÉKOZTATÓ </h2><p style="color:' + color + '>\
+ A   beleegyező   nyilatkozat   az   EU   2016/679.   számú   általános   adatvédelmi   rendelete [GDPR], \
+ valamint az információs önrendelkezési jogról és az információszabadságról szóló 2011. évi CXII. törvény rendelkezései alapján lett összeállítva. \
+ Elfogadásával a GDPR preambulum (32)-(33), 4. cikk 11. pont, 6. cikk (1)a) pont alapján hozzájárul ahhoz, \
+ hogy azon személyes adatai, amelyek az elérhetőségét biztosítják a vizsgálat   elvégzéséig   (teljes   név,   telefonszám   és   e-mail   cím),  \
+ valamint   speciális egészségügyi adatai a törvényi előírásoknak megfelelően tárolásra kerüljenek. Személyes adatok   a   résztvevők   toborzásának   \
+ folyamatához   szükségesek,   az   esetleges későbbi vizsgálatok és azok eredményei anonim módon lesznek tárolva. Személyes adataihoz csak a titoktartási \
+ megállapodást elfogadó adatkezelők férhetnek hozzá, ameddig a teljes adat nem lesz anonimizálható. A   beleegyező   nyilatkozat   elfogadásával   továbbá \
+ hozzájárul   ahhoz,   hogy   a   későbbi vizsgálat során végzett tesztek eredményei anonim módon rögzítésre kerüljenek, azt az illetékesek elektronikus úton tárolják,\
+ és kutatási célból felhasználják úgy, hogy harmadik fél   részére   személye   azonosítására   alkalmas   információt   nem   szolgáltatnak   ki. \
+ Anonimizált, személyes beazonosításra nem alkalmas adatai a tudományos közösség számára publikussá válnak a https://osf.io/  \
+ (Open Science Foundation [magyarul: Nyílt Tudomány Alapítvány]) és a https://openneuro.org/ (OpenNeuro) szerverein. \
+ A beleegyező nyilatkozatban kifejezett hozzájárulás bármikor visszavonható, amely tény nem érinti a visszavonás előtti adatkezelés jogszerűségét. \
+ A nyilvántartott személyes adatokhoz bármikor hozzáférhet, azok helyesbítését és törlését joggal kérvényezheti (GDPR 3/3/17). Továbbá jogosult arra, \
+ hogy az adatkezelőtől visszajelzést kapjon arra vonatkozóan,   hogy   személyes   adatainak   kezelése   folyamatban   van-e,   \
+ és   ha   ilyen adatkezelés folyamatban van, jogosult arra, hogy a személyes adatokhoz és a következő információkhoz   hozzáférést   kap  \
+ (GDPR 2/15/1). Kérésére   az  adatkezelő   indokolatlan késedelem nélkül helyesbíti a rá vonatkozó pontatlan személyes adatokat.\
+ Figyelembe véve az adatkezelés célját, jogosult arra, hogy kérje a hiányos személyes adatok – egyebek mellett kiegészítő nyilatkozat útján történő \
+ – kiegészítését is (GDPR 3/3/16). Adatvédelmi   panasz   esetén   joggal   fordulhat   a   Nemzeti   Adatvédelmi   és Információszabadság Hatósághoz \
+ (http://naih.hu postacím: 1530 Budapest, Pf.: 5., e-mail: ugyfelszolgalat@naih.hu, telefonszám: +36 (1) 391-1400). \
+ A fő adatkezelő elérhetősége és adatai: Név: Dr. Keresztes Attila Intézmény: Agyi Képalkotó Központ, Természettudományi Kutatóközpont\
+ Telefonszám: +36 1 3826903 E-mail cím: keresztes.attila.akk@ttk.hu </p><h2 style="color:' + color + '>BELEEGYEZŐ NYILATKOZAT </h2>\
+ <p style="color:' + color + '>A kérdőívvel és az adatkezeléssel kapcsolatban minden számomra fontos információt megkaptam, a tájékoztatót elolvastam.\
+ A kérdőívet önszántamból, saját felelősségemre töltöm ki. Kérjük,  olvassa   el  az   alábbi   állításokat!  \
+ Amennyiben   egyetért   velük, jelölje   meg  az állítások alatt található négyzetet. Tudomásul  veszem,  hogy  a  kitöltés bármikor, \
+ indoklás  nélkül, akár  végleg   is megszakítható,   és   ebben   az   esetben   a   megadott   adatok   nem   kerülnek feldolgozásra. \
+ Tudomásul veszem, hogy adataimat a 2018. május 25-től hatályos 2016/679/EU Európai Unió általános adatvédelmi irányelvét és a magyarországi jogszabályokat\
+ (2011. évi CXII. adatvédelmi tv.) is betartva fogják kezelni. Hozzájárulok   ahhoz,   hogy   a   vizsgálathoz   szükséges   személyes   adataimat, \
+ valamint a vizsgálat során rólam nyert adatokat a kísérletet végzők egymástól elkülönítve rögzítsék, azokat egymástól elkülönítve, elektronikus úton tárolják, \
+ és kutatási   célból   felhasználják,   úgy,   hogy   harmadik   fél   részére   személyem azonosítására alkalmas információt nem szolgáltatnak ki. \
+ Hozzájárulok, hogy anonimizált, személyes beazonosításomra nem alkalmas adataimat a tudományos közösséggel megosszák a https://osf.io/ \
+ (Open Science Foundation [magyarul: Nyílt Tudomány Alapítvány]) és a https://openneuro.org/ (OpenNeuro) szerverein.</p>'
+
